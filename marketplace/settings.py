e16806fe7f17637a4171e3cc2fb02fbd52e32cc1
ytgTonz <83579339+ytgTonz@users.noreply.github.com>
2025-08-01 12:13:29 +0200

    fixed syntax error in marketplace/settings.py
    commit by: ytgTonz

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-^*zoqn&yi*s-l=j)inpnp#zv!pjd42sle8ba!wm!d5w5in04@n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False'),.lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
 
